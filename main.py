50c51a4fb773536ce5d069e7e6bbefc67d9a9d1f
albaguadev <albaguadev@gmail.com>
2026-04-20 17:33:29 +0200

    Implemented a simple task manager.

---
+++ main.py
@@ -0,0 +1,41 @@
+from task_manager import TaskManager
+
+def print_menu():
+    print("\n--- Task Manager ---\n")
+    print("1. Add Task")
+    print("2. Delete Task")
+    print("3. Complete Task")
+    print("4. List Tasks")
+    print("5. Exit")
+
+def main():
+
+  task_manager = TaskManager()
+
+  while True:
+
+    print_menu()
+
+    choice = input("Choose an option: ")
+
+    match choice:
+      case "1":
+        description = input("Enter task description: ")
+        task_manager.add_task(description)
+      case "2":
+        id = input("Enter task ID to delete: ")
+        task_manager.delete_task(id)
+      case "3":
+        id = input("Enter task ID to complete: ")
+        task_manager.complete_task(id)
+      case "4":
+        task_manager.list_tasks()
+      case "5":
+        print("Exiting Task Manager. Goodbye!")
+        break
+      case _:
+        print("Invalid option. Please try again.")
+  
+
+if __name__ == "__main__":
+  main()
---
+++ task_manager.py
@@ -0,0 +1,48 @@
+class Task:
+
+    def __init__(self, id, description, completed=False):
+        self.id = id
+        self.description = description
+        self.completed = completed
+
+    def mark_completed(self):
+        self.completed = True
+
+    def __str__(self):
+        status = "Completed" if self.completed else "Pending"
+        return f"[{status}] #{self.id}: {self.description}"
+
+class TaskManager:
+
+    def __init__(self):
+        self._tasks = []
+        self._next_id = 1
+
+    def add_task(self, description):
+        task = Task(self._next_id, description)
+        self._tasks.append(task)
+        self._next_id += 1
+        print(f"Task added: {description}")
+
+    def delete_task(self, id):
+        for task in self._tasks:
+            if task.id == id:
+                self._tasks.remove(task)
+                print(f"Task deleted: {task.description}")
+                return
+        print(f"Task with ID {id} not found.")
+
+    def complete_task(self, id):
+        for task in self._tasks:
+            if task.id == id:
+                task.mark_completed()
+                print (f"Task completed: {task.description}")
+                return
+        print(f"Task with ID {id} not found.")
+
+    def list_tasks(self):
+        if not self._tasks:
+            print("No tasks available.")
+            return
+        for task in self._tasks:
+            print(task)
