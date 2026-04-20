01aa00a8dacf95caa1f764a6aeaba50b64ac7b05
albaguadev <albaguadev@gmail.com>
2026-04-20 17:40:56 +0200

    bug fixed resolved: Integer not resolved

--- main.py
+++ main.py
@@ -16,25 +16,29 @@ def main():
 
     print_menu()
 
-    choice = input("Choose an option: ")
+    try:  
+      choice = int(input("Choose an option: "))
 
-    match choice:
-      case "1":
-        description = input("Enter task description: ")
-        task_manager.add_task(description)
-      case "2":
-        id = input("Enter task ID to delete: ")
-        task_manager.delete_task(id)
-      case "3":
-        id = input("Enter task ID to complete: ")
-        task_manager.complete_task(id)
-      case "4":
-        task_manager.list_tasks()
-      case "5":
-        print("Exiting Task Manager. Goodbye!")
-        break
-      case _:
-        print("Invalid option. Please try again.")
+      match choice:
+        case 1:
+          description = input("Enter task description: ")
+          task_manager.add_task(description)
+        case 2 :
+          id = int(input("Enter task ID to delete: "))
+          task_manager.delete_task(id)
+        case 3:
+          id = int(input("Enter task ID to complete: "))
+          task_manager.complete_task(id)
+        case 4:
+          task_manager.list_tasks()
+        case 5:
+          print("Exiting Task Manager. Goodbye!")
+          break
+        case _:
+          print("Invalid option. Please try again.")
+
+    except ValueError:
+      print("Invalid option. Please try again.")
   
 
 if __name__ == "__main__":
