6c3aeb517ff7ee536701523517d39fa69cbe8614
albaguadev <albaguadev@gmail.com>
2026-04-21 12:25:58 +0200

    added Persistence with JSON

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,5 @@
+import json
+
 class Task:
 
     def __init__(self, id, description, completed=False):
@@ -14,21 +16,26 @@ class Task:
 
 class TaskManager:
 
+    FILENAME = "tasks.json"
+
     def __init__(self):
         self._tasks = []
         self._next_id = 1
+        self.load_tasks(self.FILENAME)
 
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id += 1
         print(f"Task added: {description}")
+        self.save_tasks(self.FILENAME)
 
     def delete_task(self, id):
         for task in self._tasks:
             if task.id == id:
                 self._tasks.remove(task)
                 print(f"Task deleted: {task.description}")
+                self.save_tasks(self.FILENAME)
                 return
         print(f"Task with ID {id} not found.")
 
@@ -37,6 +44,7 @@ class TaskManager:
             if task.id == id:
                 task.mark_completed()
                 print (f"Task completed: {task.description}")
+                self.save_tasks(self.FILENAME)
                 return
         print(f"Task with ID {id} not found.")
 
@@ -46,3 +54,16 @@ class TaskManager:
             return
         for task in self._tasks:
             print(task)
+    
+    def save_tasks(self, filename):
+        with open(filename, 'w') as file:
+            json.dump([task.__dict__ for task in self._tasks], file, indent=4)
+
+    def load_tasks(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self._tasks = [Task(item["id"], item["description"], item["completed"]) for item in data]
+                self._next_id = max([task.id for task in self._tasks], default=0) + 1
+        except FileNotFoundError:
+            self._tasks = []
